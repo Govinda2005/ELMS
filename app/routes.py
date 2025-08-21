1e1f30442d5e29eb39897430f5f539e378816d97
GUDISA GOVINDA <govindagudisa3427@gmail.com>
2025-08-21 13:18:45 +0530

    Update routes.py
    changing the route to /

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from flask_login import current_user
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('https://elms-mpfo.onrender.com/')
+@main_bp.route('/')
 def index():
     if current_user.is_authenticated:
         if current_user.is_admin():
